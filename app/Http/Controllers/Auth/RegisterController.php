75f29ebc0d8ca86e264020205d3484236e61894a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-04-29 21:41:02 -0300

    Correct password_verify validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,8 @@ class RegisterController extends Controller
             'name'  => ['required', 'string', 'max:255'],
             'phone' => ['required', 'string', 'size:11'],
             // 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'required_with:password', 'same:password'],
+            'password_verify' => ['required', 'string', 'min:8'],
         ]);
     }
 
